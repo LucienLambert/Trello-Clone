21d795b86953bd06592f9389b4f93ddea5aeeda2
vic <victor.renard10@gmail.com>
2021-05-20 16:55:13 +0200

    <build><boite de dialog lorqu'on click sur un event, enlever les cartes lorsqu'on decoche un checkbox> + refactor

--- js/calendar.js
+++ js/calendar.js
@@ -1,40 +1,63 @@
+// reécupere tableau des id des board selectionné
+function inputchecked() {
+    let boardChecked = [];
+    let checkboxes = document.querySelectorAll('input[name="board"]:checked');
+    console.log(checkboxes);
+    checkboxes.forEach((checkbox) => {
+        boardChecked.push(checkbox.value);
+    });
+
+    return boardChecked;
+}
+//couleur aléatoir pour les différent board checkbox(label)
+function colorBoard() {
+    let tablColor = []
+    $('label').each(function() {
+        var colorRandom = 'rgb(' +
+            (Math.floor(Math.random() * 256)) + ',' +
+            (Math.floor(Math.random() * 256)) + ',' +
+            (Math.floor(Math.random() * 256)) + ')';
+        let label = $(this);
+        let id = label.attr('id');
+        id = id.substring(0, id.indexOf("label"));
+        label.css('color', colorRandom);
+        tablColor.push([id, colorRandom]);
+    });
+    return tablColor
+}
+
+//premier affichage du calendar
+window.onload = () => {
+    boardTitleColor = colorBoard();
+    console.log(boardTitleColor);
+    calendar(boardTitleColor);
+}
+
+//listener sur les checkbox
 $(function() {
-    window.onload = () => {
-        // reécupere tableau des id des board selectionné
-        let checkboxes = document.querySelectorAll('input[name="board"]:checked');
-        console.log(checkboxes);
-        boardChecked = [];
-        checkboxes.forEach((checkbox) => {
-            boardChecked.push(checkbox.value);
-        });
-        console.log(boardChecked);
-        //couleur aléatoir pour les différent board checkbox(label)
-        tablColor = []
-        $('label').each(function() {
-            var colorRandom = 'rgb(' +
-                (Math.floor(Math.random() * 256)) + ',' +
-                (Math.floor(Math.random() * 256)) + ',' +
-                (Math.floor(Math.random() * 256)) + ')';
-            let label = $(this);
-            let id = label.attr('id');
-            id = id.substring(0, id.indexOf("label"));
-            label.css('color', colorRandom);
-            tablColor.push([id, colorRandom]);
-        });
-        //récuperer les carte de tout les board 
-        $.post("card/select_card_from_boards", {
-                boardChecked: boardChecked,
-                color: tablColor
-            },
-            function(data) {
-                evenements = JSON.parse(data);
-                console.log(evenements);
-                viewCalendar(evenements);
-            }
-        );
-    }
+    $('#label_checkbox').change(function() {
+        calendar(boardTitleColor);
+    });
 });
 
+//récupération des carte(evenements) des inputs sélectionné et affichage calendar
+function calendar(boardTitleColor) {
+    let checked = inputchecked();
+    console.log(checked);
+    //récuperer les carte de tout les board 
+    $.post("card/select_card_from_boards", {
+            boardChecked: checked,
+            color: boardTitleColor
+        },
+        function(data) {
+            evenements = JSON.parse(data);
+            console.log(evenements);
+            viewCalendar(evenements);
+        }
+    );
+}
+
+//calendar option
 function viewCalendar(evenements) {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
@@ -45,11 +68,9 @@ function viewCalendar(evenements) {
             center: 'title',
             right: 'dayGridMonth,timeGridWeek,listYear'
         },
+        height: 'auto',
         events: evenements,
         eventDrop: (infos) => {
-            /*if (infos.event.startStr > now()) {
-                infos.event.el.style('text-decoration:line-through');
-            }*/
             console.log(infos);
             console.log(infos.event.startStr);
             console.log(infos.oldEvent.startStr);
@@ -63,19 +84,32 @@ function viewCalendar(evenements) {
             }
         },
         eventClick: (infos) => {
-            alert('Event: ' + infos.event.title);
-            alert('Coordinates: ' + infos.jsEvent.pageX + ',' + infos.jsEvent.pageY);
-            alert('View: ' + infos.view.type);
-            /*$(this).click(function() {
-                $('#bodyCard_dialog').modal('show');
-            });*/
-            //dialogBox();
+            dialogBox(infos);
         }
     });
     calendar.render();
 }
 
-function dialogBox() {
-    $('#bodyCard_dialog').modal('show');
-
+//boite modal pour afficher la carte(evenement) 
+function dialogBox(infos) {
+    console.log(infos.event.title);
+    console.log(infos.event);
+    console.log(infos.event.extendedProps.description);
+    console.log(infos.event.startStr);
+    $('#title').html(infos.event.title);
+    $('#body').html(infos.event.extendedProps.description);
+    $('#dueDate').html(infos.event.startStr);
+    $("#modalDialog").dialog({
+        resizable: false,
+        draggable: false,
+        height: 250,
+        width: 500,
+        modal: true,
+        title: 'evenement',
+        buttons: {
+            Ok: function() {
+                $(this).dialog("close");
+            }
+        }
+    });
 }
--- view/view_calendar.php
+++ view/view_calendar.php
@@ -6,8 +6,11 @@
         <base href="<?= $web_root ?>"/>
         <link href="css/style.css" rel="stylesheet" type="text/css"/>   
         <link href='lib/fullcalendar/lib/main.css' rel='stylesheet' />
-        
+        <link href="lib/jquery-ui-1.12.1/jquery-ui.min.css" rel="stylesheet" type="text/css"/>
+    <link href="lib/jquery-ui-1.12.1/jquery-ui.theme.min.css" rel="stylesheet" type="text/css"/>
+    <link href="lib/jquery-ui-1.12.1/jquery-ui.structure.min.css" rel="stylesheet" type="text/css"/>
         <script src="lib/jquery-3.6.0.min.js" type="text/javascript"></script>
+        <script src="lib/jquery-ui-1.12.1/jquery-ui.min.js" type="text/javascript"></script>
         <script src='lib/fullcalendar/lib/main.js'></script>
         <script src="js/calendar.js" type="text/javascript"></script>
         
@@ -18,15 +21,16 @@
         <div id="label_checkbox">
             <form>
                 <?php foreach($tablBoard as $board){?>
-                    <input type="checkbox" id="<?php echo $board->getId() . 'idBoard';?>" name="board" value="<?php echo $board->getId()?>" checked>
+                    <input onchange="inputchecked()"  type="checkbox" id="<?php echo $board->getId() . 'idBoard';?>" name="board" value="<?php echo $board->getId()?>" checked>
                     <label id="<?php echo $board->getId() .'label' ?>" for="<?php echo $board->getId() ?>" ><?php echo $board->getTitle(); ?></label>
                 <?php } ?>    
             </form>
         </div>
-        <div id="calendar">
-            <div id="bodyCard_dialog" titre="Carte" hidden>
-                p>carte</p>
-            </div> 
+        <div id="calendar"></div>
+        <div id="modalDialog" hidden>
+            <h5>TitleCard : <span id="title"> </span></h>
+            <h5>Body : <span id="body"> </span></h>
+            <h5>DueDate : <span id="dueDate"> </span></h>
         </div>
     </body>
 </html>
